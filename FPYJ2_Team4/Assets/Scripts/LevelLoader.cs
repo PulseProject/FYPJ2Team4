59a2b2597313ce9a9d37d61ff9ef8d72f416425f
Eikatsu <hayashieikata@hotmail.co.jp>
2015-05-19 00:09:43 +0800

    Done List: - New Branch - New Menu - Basic Transitions

---
+++ FPYJ2_Team4/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+
+	/*// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+	
+	public void NextLevelButton (int index)
+	{
+		Application.LoadLevel(index);
+	}
+	public void NextLevelButton(string levelName)
+	{
+		Application.LoadLevel(levelName);
+	}
+}
---
+++ FPYJ2_Team4/Assets/Scripts/TextFlasher.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TextFlasher : MonoBehaviour {
+
+	//variables
+	Text flashingStartText;
+	public string textFlash;
+	string blink = "";
+	bool blinkSwitch = true;
+
+	// Use this for initialization
+	void Start () {
+		flashingStartText = GetComponent<Text>();
+		StartCoroutine(Blinking());
+	}
+
+	public IEnumerator Blinking(){
+		while(blinkSwitch){
+			flashingStartText.text = blink;
+			yield return new WaitForSeconds(.5f);
+			flashingStartText.text = textFlash;
+			yield return new WaitForSeconds(.5f);
+		}
+	}
+
+	/*// Update is called once per frame
+	void Update () {
+	
+	}*/
+}
