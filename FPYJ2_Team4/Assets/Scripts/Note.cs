c52b0b3b0c6cf54503dc7be7b41263391a9904a7
Marvin <mjgt082896@gmail.com>
2015-05-19 10:57:56 +0800

    FIXED DESTROY NOTE WHEN NOT VISIBLE

--- FPYJ2_Team4/Assets/Scripts/Note.cs
+++ FPYJ2_Team4/Assets/Scripts/Note.cs
@@ -5,18 +5,20 @@ using System.Collections;
 //Script for the Red Note and only the Red Note!!
 
 public class Note : MonoBehaviour {
-
+	
 	// Use this for initialization
+	private GameObject RED;
 	void Start () 
-    {
-
+	{
+		RED = GameObject.Find ("Note");
 	}
 	
 	// Update is called once per frame
 	void Update () 
-    {
+	{
 		transform.Translate(new Vector3(0, -5, 0) * Time.deltaTime);
-		if (!gameObject.GetComponent<Renderer> ().isVisible)
+		
+		if (!GetComponent<Renderer> ().isVisible)
 		{
 			Destroy(gameObject);
 			Debug.Log("Destroyed");
--- FPYJ2_Team4/Assets/Scripts/Play_Song.cs
+++ FPYJ2_Team4/Assets/Scripts/Play_Song.cs
@@ -3,39 +3,37 @@ using System.Collections;
 
 public class Play_Song : MonoBehaviour 
 {
-    public GameObject RedNote;
-    public GameObject Blue;
-    public GameObject Yellow;
-    public GameObject Green;
-
-    public int part = 0;
+	public GameObject RedNote;
+	public GameObject Blue;
+	public GameObject Yellow;
+	public GameObject Green;
+	
+	public int part = 0;
 	// Use this for initialization
 	void Start () 
-    {
+	{
 		InvokeRepeating ("SongStart", 2, 1);
 		//if (part <= 1) {
 		//SongStart ();
-			
+		
 		//	return;
 		//}
 	}
-    IEnumerator NoteDelay()
-    {
-        //a = 0;
-        yield return new WaitForSeconds(2.0f);
-    }
+	IEnumerator NoteDelay()
+	{
+		//a = 0;
+		yield return new WaitForSeconds(2.0f);
+	}
 	// Update is called once per frame
 	void SongStart () 
-    {
+	{
 		StartCoroutine(NoteDelay());
 		Instantiate(RedNote, new Vector3 (-1.5f, 5.0f, 0), Quaternion.identity);
 		StartCoroutine(NoteDelay());
-		Instantiate(Yellow, new Vector3 (1.5f, 5.0f, 0), Quaternion.identity);
-		StartCoroutine(NoteDelay());
-
-
-        part += 1;
+		
+		
+		part += 1;
 		Debug.Log ("Part: " + part);
-	
+		
 	}
 }
